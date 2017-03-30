02884d851fb5798d75eda64a73ad5f6d673e6233
Kalin Gyokov <kalin_gyokov@hotmail.com>
2017-03-29 21:15:06 -0700

    Implementing various class types

--- priv/src/Gauge.purs
+++ priv/src/Gauge.purs
@@ -1,52 +1,61 @@
 module Gauge where
   
 import Prelude
-import Control.Bind ((=<<))
-import Control.Monad.Eff.Exception
 import Data.Maybe
-import Data.Monoid
-import Data.Functor
-import Data.List as L
+import Data.Monoid (class Monoid, mempty)
+import Data.Array
+import Data.Tuple (Tuple(..))
+import Data.Time (Time)
 import Data.Map as Map
-import Data.Eq (Eq)
-import Data.Foldable (sum)
-import Data.List (concatMap)
-import Data.Map (Map, alter, fromFoldable, update)
+import Data.Map (Map)
 
 import Pux.Html.Events (onClick)
 import Pux.Html as H
-import Pux.CSS as CSS
-import Pux.Html.Attributes as A
+--import Pux.CSS as CSS
 import Pux.Html.Events as E
 
-import React.DOM.Props (onChange)
+data ViewType = PerNode | PerBroker | PerNodeAndBroker | Aggregate
 
-data ViewType = PerNode | PerBroker | PerNodeAndBroker | Aggregate deriving Eq
+derive instance viewTypeEq :: Eq ViewType
 
-data Action = UpdateStats (List NodeStats) | ChangeView ViewType
+data Action = UpdateStats (Array NodeStats) | ChangeView ViewType
 
 type TimedVal a = { time::Time, value::a }
 type State = 
     { viewType:: ViewType
-    , stats:: List NodeStats }
+    , stats:: Array NodeStats }
 
 -- /todo: Add Node = String | IPAddress
-type BStats = Int
+newtype BStats = BStats Int
+instance bstatsSemigroup :: Semigroup BStats where
+    append (BStats b1) (BStats b2 ) = BStats (b1 + b2)
+instance bstatsMonoid :: Monoid BStats where
+    mempty = BStats 0
+instance bstatsShow :: Show BStats where
+    show (BStats b) = show b
+
 type Node = String
 
-type Broker = 
-    { name :: Node
-    , port :: Int }
+newtype Broker = Broker { name :: Node, port :: Int }
+
+derive instance brokerEq :: Eq Broker
+derive instance brokerOrd :: Ord Broker 
+
+instance brokerShow :: Show Broker where
+    show (Broker {name,port}) = show name <> ":" <> show port
+-- instance brokerOrd :: Ord Broker where
+--     compare (Broker {name:n1, port:p1}) (Broker {name:n2, port:p2}) = (compare n1 n2) <> (compare p1 p2)
+
 type BrokerStats = 
     { broker ::Broker
     , connections :: BStats}
 
 type NodeStats =  
     { node ::  Node
-    , brokers:: List BrokerStats}
+    , brokers:: Array BrokerStats}
 
 init :: State
-init = { viewType : PerBroker, stats : Nil }
+init = { viewType : PerBroker, stats : [] }
 
 update :: Action -> State -> State
 update (UpdateStats us) s = s {stats = us}
@@ -59,51 +68,52 @@ view {viewType,stats} =
         , select viewType 
         , stats # viewAs viewType ]
 
-numConnectionsView :: (List NodeStats) -> H.Html Action
+numConnectionsView :: (Array NodeStats) -> H.Html Action
 numConnectionsView stats = 
-    H.text $ stats # show <<< numConnections
+    H.text $ stats # numConnections >>> show
 
-viewAs :: ViewType -> (List NodeStats) -> H.Html Action
+viewAs :: ViewType -> (Array NodeStats) -> H.Html Action
 viewAs PerNode s = 
-    H.div [] 
-    s # map \{node,brokers} ->
-        let agg = brokers # map _.connections # sum in
+    H.div []
+    (s # map \{node,brokers} ->
+        let agg = brokers # map _.connections >>> fold in
         H.div [] 
-            [ H.h1  [] [text $ show node]
-            , viewStats agg ]
+            [ H.h1  [] [H.text $ show node]
+            , viewStats agg ])
 
 viewAs PerBroker s =
-    let mergeBrokerStats {broker, connections} = addOrInsert broker connections
+    let 
+        mergeBrokerStats m {broker, connections} = addOrInsert connections broker m
         statsPerBroker = s # concatMap _.brokers 
-                            >>> foldl mergeBrokerStats Map.empty
-                            >>> Map.toAscUnfoldable
+                           >>> foldl mergeBrokerStats Map.empty
+                           >>> Map.toAscUnfoldable
     in
     H.div []
-    statsPerBroker
-    # map \(Tuple broker agg) ->
+    (statsPerBroker # map \(Tuple broker agg) ->
          H.div [] 
-            [ H.h1  [] [text $ show broker]
-            , viewStats agg]
+            [ H.h1  [] [H.text $ show broker]
+            , viewStats agg])
 
 viewAs PerNodeAndBroker s = 
-    H.div []
-    s # concatMap \{node,brokers} -> brokers # map {node,brokerStats}
-    >>> map \{node,brokerStats} ->
-        [ H.h1  [] [text $ "Node: " <> node <> " Broker: " <> show brokerStats.broker]
-            , viewStats brokerStats.connections]
+    H.div [] $
+    s # concatMap (\{node,brokers} -> brokers # map \brokerStats -> {node,brokerStats})
+    >>> (map \{node,brokerStats} ->
+        H.div []
+        [ H.h1  [] [H.text $ "Node: " <> node <> " Broker: " <> show brokerStats.broker]
+            , viewStats brokerStats.connections])
 
 viewAs Aggregate s = 
-    let agg = s # numConnections in
+    let agg = numConnections s in
     H.div [] 
-        [ H.h1  [] [text "Aggregate Stats"]
+        [ H.h1  [] [H.text "Aggregate Stats"]
         , viewStats agg]
 
 
 viewStats :: BStats -> H.Html Action
 viewStats stats = 
     H.div [] 
-        [ H.h2 [] [text "Connections:"]
-        , H.p  [] [text $ show stats] ]
+        [ H.h2 [] [H.text "Connections:"]
+        , H.p  [] [H.text $ show stats] ]
 
 select :: ViewType -> H.Html Action
 select viewType = 
@@ -113,9 +123,7 @@ select viewType =
 optionView :: ViewType -> ViewType -> H.Html Action
 optionView sel v =
     H.li 
-        if v == sel 
-        then [CSS.style ""]
-        else [E.onClick $ const (ChangeView v)]
+        (if v == sel then [] else [E.onClick $ const (ChangeView v)])
         [H.text $ show v]
 
 availableViews :: Array ViewType
@@ -128,17 +136,19 @@ instance viewTypeShow :: Show ViewType where
   show PerNodeAndBroker = "Per Node And Broker"
   show Aggregate = "Aggregate"
 
-numConnections :: List NodeStats -> Int
+numConnections :: Array NodeStats -> BStats
 numConnections = 
     concatMap _.brokers
     >>> map _.connections
-    >>> sum
+    >>> fold
 
-upsert :: forall k v. Monoid v => (v -> v) -> k -> Map k v -> Map k v
+upsert :: forall k v. (Monoid v, Ord k) => (v -> v) -> k -> Map k v -> Map k v
 upsert f = Map.alter (emptyIfNothing >>> f >>> Just)
 
-addOrInsert :: forall k v. Monoid v => v -> k -> Map k v -> Map k v
-addOrInsert v = upsert $ (<#>) v
+updateJust f = emptyIfNothing >>> f >>> Just
+
+addOrInsert :: forall k v. (Monoid v, Ord k) => v -> k -> Map k v -> Map k v
+addOrInsert v = upsert $ (<>) v
 
 emptyIfNothing :: forall f. Monoid f => Maybe f -> f
 emptyIfNothing (Just fa) = fa
