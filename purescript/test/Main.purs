3c8584e5b7f32f3dea4c5f02333bdff942e5adf4
Kalin Gyokov <kalin_gyokov@hotmail.com>
2017-05-14 01:24:07 -0700

    Rewriting tests using purescript-test-unit

--- purescript/test/Main.purs
+++ purescript/test/Main.purs
@@ -2,24 +2,29 @@ module Test.Main where
 
 import Prelude
 import Main
-import Test.Assert
+
+import Test.Unit (suite, test, timeout)
+import Test.Unit.Main (runTest)
+import Test.Unit.Assert as Assert
+
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION, Error, catchException, throwException)
 
-main :: forall e. Eff (assert::ASSERT, err::EXCEPTION, console :: CONSOLE | e) Unit
-main = do
-  convertToWsUrlTests
+--main :: forall e. Eff (assert::ASSERT, err::EXCEPTION, console :: CONSOLE | e) Unit
+main = runTest do
+  suite "convertToWsUrl" do
+    test "Port+Path+Query" do
+      url1 <- liftEff $ convertToWsUrl "http://localhost:12000/static/index.html?q=123"
+      Assert.equal url1 "ws://localhost:12000/ws/stats"
+    test "Port+EmptyPath" do        
+      url2 <- liftEff $ convertToWsUrl "http://test.org:80/"
+      Assert.equal url2 "ws://test.org:80/ws/stats"
+    test "Port" do
+      url3 <- liftEff $ convertToWsUrl "http://test.org:80"
+      Assert.equal url3 "ws://test.org:80/ws/stats"
 
 
-convertToWsUrlTests :: forall e. Eff (assert::ASSERT, console :: CONSOLE, err::EXCEPTION | e) Unit
-convertToWsUrlTests = do 
-  url1 <- convertToWsUrl "http://localhost:12000/static/index.html?q=123"
-  assert (url1 == "ws://localhost:12000/ws/stats")
-  url2 <- convertToWsUrl "http://test.org:80/"
-  assert (url2 == "ws://test.org:80/ws/stats")
-  url3 <- convertToWsUrl "http://test.org:80"
-  assert (url3 == "ws://test.org:80/ws/stats")
-  --(catchException ((\_ -> log "Exception caught as expected"):: Error -> Eff (console :: CONSOLE | e) Unit) ((const unit) <$> (convertToWsUrl "./test.org:80")))
   
 
